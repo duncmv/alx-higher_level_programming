ff03cadfe8c8243b1b3ef9e8b50fb88ce855f85d
Duncan Asiimwe <dtindiwensi@gmail.com>
2023-10-30 10:58:00 -0700

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,15 +8,15 @@ int check_cycle(listint_t *list)
 {
 	listint_t *temp, *check;
 
-	for (temp = list; temp != NULL; temp = temp->next)
+	for (temp = list, check = list; temp != NULL; temp = temp->next)
 	{
-		check = list;
-	       	while(check != NULL) 
+	       	do 
 		{
 			if (temp->next == check)
 				return (1);
 			check = check->next;
-		}
+		} while (check != temp && check != NULL);
+		check = list;
 	}
 	return (0);
 }
