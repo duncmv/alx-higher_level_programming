c95f64c24757f523eb21af8c580157dd1c531329
Duncan Asiimwe <dtindiwensi@gmail.com>
2023-10-31 03:43:02 -0700

    mandatory tasks

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@
 def fizzbuzz():
     for n in range(1, 101):
         if n % 3 == 0 and n % 5 == 0:
-            print("Fizzbuzz ", end='')
+            print("FizzBuzz ", end='')
         elif n % 3 == 0 or n % 5 == 0:
             print("{} ".format("Fizz" if n % 3 == 0 else "Buzz"), end='')
         else:
