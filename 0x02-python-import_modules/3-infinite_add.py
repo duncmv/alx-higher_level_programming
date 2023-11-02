b0938d488bd008a03fbe0ebd5923568e8855b7c8
Duncan Asiimwe <dtindiwensi@gmail.com>
2023-11-02 02:48:10 -0700

    Mandatory tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    ac = len(argv) - 1;
-    i , total = 1, 0
+    ac = len(argv) - 1
+    i, total = 1, 0
     while i <= ac:
         total = total + int(argv[i])
         i = i + 1
