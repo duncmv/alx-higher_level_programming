a943f0012657939557cc5050f6061c58b1071fff
Duncan Asiimwe <dtindiwensi@gmail.com>
2023-11-04 12:37:29 -0700

    mandatory tasks

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     if lis == 0:
         return
     m = my_list[1]
-    for n in my_list[1:]:
+    for n in my_list:
         if n > m:
             m = n
     return m
