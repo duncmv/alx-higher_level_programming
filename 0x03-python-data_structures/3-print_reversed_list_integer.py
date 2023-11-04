ad01a6323432ee6d1a8eb82d06fa08e54ceb8c34
Duncan Asiimwe <dtindiwensi@gmail.com>
2023-11-04 12:49:35 -0700

    mandatory tasks

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return
     for n in reversed(my_list):
-        print("{:d}".format(my_list[n]))
+        print("{:d}".format(n))
