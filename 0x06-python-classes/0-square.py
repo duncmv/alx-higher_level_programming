0f6f47ff5ad2cc7faa6de35b767e36470852258a
duncmv <dtindiwensi@gmail.com>
2023-11-21 15:00:53 +0300

    tasks

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -5,6 +5,7 @@ This is the "Square"  module.
 This module provides a simple Square class.
 """
 
+
 class Square:
     """This is an empty class Square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,9 +3,11 @@
 This is the "Square"  module.
 
 This module provides a simple Square class.
+
 """
+
 
 class Square:
     """This a class that defines a Square by size"""
     def __init__(self, size):
-        self.__size = size 
+        self.__size = size
