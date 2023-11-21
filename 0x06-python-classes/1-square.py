ece65a925fc9e089584582ead9ea31e0a56e84e7
duncmv <dtindiwensi@gmail.com>
2023-11-21 14:53:11 +0300

    tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """This a class Square"""
+    def __init__(self, size) -> None:
+        """
+        Args:
+            size: size of square
+        """
+        self.__size = size  #: size of square
