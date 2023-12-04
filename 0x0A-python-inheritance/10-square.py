3144baff74b334b1d91e53aa6def753f6a5af98c
duncmv <dtindiwensi@gmail.com>
2023-12-04 13:21:24 +0300

    tasks

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """Square class that inherits Rectangle"""
 
 
+Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size):
