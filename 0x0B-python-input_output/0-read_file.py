99c14bf87b6cffb0841c7f72071c77fc60c04179
duncmv <dtindiwensi@gmail.com>
2023-12-05 14:42:19 +0300

    tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """reads a file"""
-    with open(filename, encoding="UTF-8") as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
         print(f.read())
