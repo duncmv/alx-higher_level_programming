2270b0164f10a03bbd0b682def41d42f14590bc1
duncmv <dtindiwensi@gmail.com>
2023-12-07 18:43:36 +0300

    task 21 - draw

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@
 
 import json
 import os.path
+import turtle
 
 
 class Base:
@@ -62,6 +63,7 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
+        """saves objects in csv"""
         with open(f"{cls.__name__}.csv", "w", encoding="utf-8") as f:
             for i in list_objs:
                 csv_str = f"{i.id},"
@@ -74,14 +76,87 @@ class Base:
 
     @classmethod
     def load_from_file_csv(cls):
+        """loads objects from csv"""
         insts = []
         if os.path.isfile(f"{cls.__name__}.csv"):
             with open(f"{cls.__name__}.csv", "r", encoding="utf-8") as f:
                 for line in f:
                     if cls.__name__ == "Rectangle":
-                        id, width, height, x, y = map(int, list(line.split(",")))
+                        id, width, height, x, y = map(int, list(line.split(
+                            ",")))
                         insts.append(cls(width, height, x, y, id))
                     elif cls.__name__ == "Square":
                         id, size, x, y = map(int, list(line.split(",")))
                         insts.append(cls(size, x, y, id))
         return insts
+
+    @staticmethod
+    def draw_axes(pen):
+        """Draw X and Y axes and mark them every 20 pixels"""
+        pen.up()
+        pen.goto(-200, 0)
+        pen.down()
+        pen.goto(200, 0)
+        pen.up()
+        pen.goto(0, -200)
+        pen.down()
+        pen.goto(0, 200)
+        pen.up()
+        for i in range(-180, 201, 20):
+            if i != 0:
+                pen.goto(i, -3)
+                pen.down()
+                pen.goto(i, 3)
+                pen.up()
+
+                pen.goto(-3, i)
+                pen.down()
+                pen.goto(3, i)
+                pen.up()
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draws the objects
+        - takes size, width, height as number of pixels
+        - takes x and y as coordinates relative to origin(0,0)
+        - prints objects in different colors
+        with its top-left vertex at the (x,y) coordinate
+        - The axes are marked every 20 pixels for verification
+        """
+
+        window = turtle.Screen()
+        window.title("Rectangle and Square Drawing")
+
+        pen = turtle.Turtle()
+        pen.hideturtle()
+        pen.speed('fastest')
+
+        Base.draw_axes(pen)
+
+        pen.speed('normal')
+        colors = ["red", "blue", "green", "yellow", "purple", "orange"]
+        color_index = 0
+        for rect in list_rectangles:
+            pen.color(colors[color_index % len(colors)])
+            Base.draw_shape(pen, rect.x, rect.y, rect.width, rect.height)
+            color_index += 1
+
+        for square in list_squares:
+            pen.color(colors[color_index % len(colors)])
+            Base.draw_shape(pen, square.x, square.y, square.size, square.size)
+            color_index += 1
+
+        turtle.done()
+
+    @staticmethod
+    def draw_shape(pen, x, y, width, height):
+        """helper function for draw"""
+        pen.up()
+        pen.goto(0, 0)
+        pen.goto(x, y)
+        pen.down()
+        for _ in range(2):
+            pen.forward(width)
+            pen.right(90)
+            pen.forward(height)
+            pen.right(90)
