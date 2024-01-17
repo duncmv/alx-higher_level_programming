07939ef8cba622e10ff05c776aac8b8c2a6ae2a4
duncmv <dtindiwensi@gmail.com>
2024-01-17 20:10:25 +0300

    mandatory

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,7 +2,6 @@
 -- Each record show display tv_shows.title, tv_show_genres.genre_id
 -- If show doesn't have genre, display NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-NATURAL JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC;
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
