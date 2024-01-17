377cc2342996f276196f443a16b6dd49b17f9ff7
duncmv <dtindiwensi@gmail.com>
2024-01-17 12:21:57 +0300

    mandatory

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,4 @@
 -- Change to the database
 USE hbtn_0c_0;
-
--- Convert the database character set and collation
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
 -- Convert the table character set and collation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the field character set and collation
-ALTER TABLE first_table CHANGE name name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
