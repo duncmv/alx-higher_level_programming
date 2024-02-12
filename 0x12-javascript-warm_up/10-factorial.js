f1a9e4e8d00255245ce43517520b62f103fe82cb
duncmv <dtindiwensi@gmail.com>
2024-02-12 17:05:55 +0300

    mandatory

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const num1 = parseInt(process.argv[2]);
-if (isNaN(num1)) {
-  console.log(1);
-}
+
 function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
   if (n === 0) {
     return 1;
   }
