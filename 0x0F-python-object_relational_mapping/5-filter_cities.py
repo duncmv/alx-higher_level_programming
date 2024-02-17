4b18d7011dfec32afa881750b95a4f0e66e5dd0b
duncmv <dtindiwensi@gmail.com>
2024-02-17 20:43:43 +0300

    mysqldb

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     query = """SELECT cities.name
                 FROM cities
-                JOIN states ON cities.state_id = states.id
-                WHERE BINARY states.name = %s"""
+                INNER JOIN states ON cities.state_id = states.id
+                WHERE states.name = %s"""
     name = (argv[4],)
     cur.execute(query, name)
     rows = cur.fetchall()
