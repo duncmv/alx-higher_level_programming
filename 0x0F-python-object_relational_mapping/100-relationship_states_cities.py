aadac268342553a2d43956b0e909aa6b22ea506b
duncmv <dtindiwensi@gmail.com>
2024-02-18 15:00:57 +0300

    correction

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
     from relationship_state import Base, State
     from relationship_city import City
     from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import Session
+    from sqlalchemy.orm import sessionmaker
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     cali = State(name="California")
     cali.cities.append(City(name="San Fancisco"))
     session.add(cali)
