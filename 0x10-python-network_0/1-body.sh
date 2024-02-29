c92d57d05f790d5d5f9bedc69f5c7ad48177a5b9
duncmv <dtindiwensi@gmail.com>
2024-02-29 14:52:14 +0300

    tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the body of only 200 status code
-curl -s -w "%{http_code} %{remote_ip}\n" "$1" | tail -n 1 | awk '{if($1==200) system("curl -s " $2) }'
+ip=$1; curl -s -w "%{http_code}" "$ip" | tail -n 1 | awk -v ip="$ip" '{if($0==200) system("curl -s " ip) }'
