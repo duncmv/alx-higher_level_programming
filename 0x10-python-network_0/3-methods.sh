9416bab05b4332c65dbb0ba3b970d3d2fa7eb40d
duncmv <dtindiwensi@gmail.com>
2024-02-29 15:59:37 +0300

    tasks

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the size of the body of response from a url
-curl -sI -X OPTIONS "$1" | grep -i Allow | awk '{print $2}'
+curl -sLI -X OPTIONS "$1" | grep -i Allow | awk '{print $2}'
