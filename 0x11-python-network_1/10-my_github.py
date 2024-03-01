fee5a2db2ff3aee4f077e2fd598b96bb0c269f4f
duncmv <dtindiwensi@gmail.com>
2024-03-01 16:01:22 +0300

    tasks

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""script that takes your GitHub credentials (username and password)
+uses the GitHub API to display your id"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = f'http://api.github.com/users/{sys.argv[1]}'
+    head = {'Authorization': f'token {sys.argv[2]}'}
+    resp = requests.get(url, headers=head)
+    print(resp.json()['id'])
