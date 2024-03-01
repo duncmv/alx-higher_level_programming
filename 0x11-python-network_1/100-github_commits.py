3cdd8d79009ca1dcba189c91948f17772303cc3a
duncmv <dtindiwensi@gmail.com>
2024-03-01 16:32:57 +0300

    tasks

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that takes your GitHub credentials (username and password)
+uses the GitHub API to display your id"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = f'http://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits'
+    resp = requests.get(url)
+    commits = resp.json()
+    for i in range(10):
+        com = commits[i]
+        print(f'{com["sha"]}: {com["commit"]["author"]["name"]}')
