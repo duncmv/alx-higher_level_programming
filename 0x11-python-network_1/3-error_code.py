4538d285b040ddcbb8a0eedddc4046f8ea511098
duncmv <dtindiwensi@gmail.com>
2024-03-01 13:56:11 +0300

    tasks

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script that takes in a URL , sends a request to the URL
+displays the body of response"""
+import urllib.request
+import urllib.parse
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as resp:
+            print(resp.read())
+    except urllib.error.HTTPError as e:
+        print(f'Error code: {e.code}')
