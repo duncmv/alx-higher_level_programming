acf350a35edc5dcd11a9a08603e66c6c2a537c6c
duncmv <dtindiwensi@gmail.com>
2024-03-01 13:43:23 +0300

    tasks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import sys
 
 
 if __name__ == "__main__":
-    data = {'email' : sys.argv[2]}
-    data = urllib.parse.urlencode(data)
+    data = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(data).encode('utf-8')
     with urllib.request.urlopen(sys.argv[1], data=data) as resp:
-        print(resp.read())
+        print(resp.read().decode('utf-8'))
